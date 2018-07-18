edac38e3d8c04777bcfb235d055dae88d65f9160
James Pyne <james.pyne.13@ucl.ac.uk>
2018-07-18 14:16:50 +0100

    Scripts added.

---
+++ inference.py
@@ -0,0 +1,223 @@
+'''
+This file performs the inference based on the learned mode
+parameters (checkpoint). Each image is cut into 4 quarters due
+to the limit of gpu memory. Vertical and horizonta reflections,
+and [0, 90, 180, 270] degrees rotations are performed for each
+quarter, and the outputs of sigmoid predictions are later
+arithmetically averaged.
+'''
+
+import tensorflow as tf
+import simplejson
+from utils import data_utils, train_utils
+import os
+import numpy as np
+import cv2
+import train
+import pandas as pd
+from shapely import wkt
+import time
+import sys
+
+
+
+def test_input(img, img_size, H):
+    '''
+    This function cuts each image into 4 quarters:
+    ([upper, lower] * [left, right]), performs vertical and horizontal
+    reflections, and [0, 90, 180, 270] degrees rotations for each quarter.
+    It yields (4 * 2 * 4 =)32 images.
+    :param img:
+    :param img_size:
+    :return:
+    '''
+    [img_width, img_height] = img_size
+    [crop_width, crop_height] = H['crop_size']
+    pad = H['pad']
+    x_width = H['x_width']
+    x_height = H['x_height']
+
+    for [x_start, x_end, y_start, y_end] in [
+        [0, crop_width, 0, crop_height],
+        [0, crop_width, crop_height, img_height],
+        [crop_width, img_width, 0, crop_height],
+        [crop_width, img_width, crop_height, img_height],
+    ]:
+        feature = img[x_start: x_end, y_start:y_end, :]
+        for feat_trans in [feature, np.rollaxis(feature, 1, 0)]:
+            for [x_step, y_step] in [[1, 1], [-1, 1], [1, -1], [-1, -1]]:
+                feature_w_padding = cv2.copyMakeBorder(
+                    feat_trans[::x_step, ::y_step, :],
+                    pad, x_width - pad - feat_trans.shape[0],
+                    pad, x_height - pad - feat_trans.shape[1],
+                    cv2.BORDER_REFLECT_101)
+                yield feat_trans.shape, feature_w_padding
+
+
+
+def pred_for_each_quarter(sess, img_in, pred, img_data, H):
+    '''
+
+    :param sess:
+    :param img_in:
+    :param pred:
+    :param img_data:
+    :param H:
+    :return:
+    '''
+    num_channel = H['num_channel']
+    x_width = H['x_width']
+    x_height = H['x_height']
+    batch_size = H['batch_size']
+
+    mask_stack, shape_stack = [], []
+    for feat_shape, img in test_input(
+            img_data.train_feature, img_data.image_size, H):
+        predictions, = sess.run(
+            [pred],
+            feed_dict={img_in: np.reshape(img,
+                                          [batch_size,
+                                           x_width,
+                                           x_height,
+                                           num_channel])})
+        mask_stack.append(predictions)
+        shape_stack.append(feat_shape)
+    return mask_stack, shape_stack
+
+def stitch_mask(img_stack, img_size, feat_shape, H):
+    '''
+    img_stack is the stack of pixel-wise inference of input images from
+    test_input. This function reverts the reflection and rotations and
+    stitches the 4 quarters together.
+    :param img_stack:
+    :param img_size:
+    :param feat_shape:
+    :return:
+    '''
+    mask = np.zeros([8, img_size[0], img_size[1]])
+    [img_width, img_height] = img_size
+    [crop_width, crop_height] = H['crop_size']
+    pad = H['pad']
+
+    idx = 0
+    for [x_start, x_end, y_start, y_end] in [
+        [0, crop_width, 0, crop_height],
+        [0, crop_width, crop_height, img_height],
+        [crop_width, img_width, 0, crop_height],
+        [crop_width, img_width, crop_height, img_height],
+    ]:
+        quarter = 0
+        for feat_trans in range(2):
+            for [x_step, y_step] in [[1, 1], [-1, 1], [1, -1], [-1, -1]]:
+
+                img_stack[idx] = img_stack[idx] \
+                    [pad: pad + feat_shape[idx][0],
+                                 pad: pad + feat_shape[idx][1]]
+
+                img_stack[idx] = img_stack[idx][::x_step, ::y_step]
+
+                if feat_trans == 1:
+                    img_stack[idx] = np.rollaxis(img_stack[idx], 1, 0)
+
+                mask[quarter, x_start: x_end, y_start: y_end] = img_stack[idx]
+
+                quarter += 1
+                idx += 1
+
+    return np.squeeze((np.mean(mask, axis=0) > 0.5).astype(np.int))
+
+
+
+if __name__ == '__main__':
+    hypes = './hypes/hypes.json'
+    with open(hypes, 'r') as f:
+
+        H = simplejson.load(f)
+
+        H['batch_size'] = 1
+        H['pad'] = 100
+        H['x_width'] = 1920
+        H['x_height'] = 1920
+        H['print_iter'] = 100
+        H['save_iter'] = 500
+        H['crop_size'] = [1700, 1700]
+
+        print_iter = H['print_iter']
+        num_channel = H['num_channel']
+        x_width = H['x_width']
+        x_height = H['x_height']
+        batch_size = H['batch_size']
+        class_type = H['class_type']
+        pad = H['pad']
+        class_type = H['class_type']
+        log_dir = H['log_dir']
+        save_iter = H['save_iter']
+        # Crop area for each inference, and this is limited by memory of k80 gpu
+        [crop_width, crop_height] = H['crop_size']
+
+    img_in = tf.placeholder(dtype=tf.float32,
+                            shape=[batch_size, x_width, x_height, 16])
+    logits, pred = train.build_pred(img_in, H, 'test')
+
+    sys.stdout.write('\n')
+    sys.stdout.write('#' * 80 + '\n')
+    sys.stdout.write("Preparing submission file for class type {}".\
+                     format(class_type).ljust(55, '#').rjust(80, '#') + '\n')
+    sys.stdout.write('#' * 80 + '\n')
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    saver = tf.train.Saver()
+
+    df = pd.read_csv('data/sample_submission.csv')
+
+    if not os.path.exists('./submission'):
+        os.makedirs('./submission')
+
+    with tf.Session(config=config) as sess:
+
+        saver.restore(sess, save_path= 'log_dir/path_to_ckpt/ckpt/ckpt-9000')
+        start_time = time.time()
+        sys.stdout.write('\n')
+
+        for idx, row in df.iterrows():
+
+            if row[1] == class_type + 1:
+                img_id = data_utils.test_IDs_dict_r[row[0]]
+                img_data = data_utils.ImageData(img_id, phase='test')
+                img_data.load_image()
+                img_data.create_train_feature()
+
+                mask_stack, shape_stack = pred_for_each_quarter(
+                    sess, img_in, pred, img_data, H)
+                mask = stitch_mask(mask_stack, img_data.image_size, shape_stack, H)
+
+                polygons = data_utils.mask_to_polygons(
+                    mask=mask, img_id=img_id, test=True, epsilon=1)
+
+                df.iloc[idx, 2] = \
+                    wkt.dumps(polygons) if len(polygons) else 'MULTIPOLYGON EMPTY'
+
+            if idx % print_iter == 0:
+                str1 = 'Working on Image No. {} Class {}: '.format(idx, class_type)
+                str2 = 'Time / image: {0:.2f} (mins); '. \
+                    format((time.time() - start_time) / 60. / print_iter \
+                                                      if idx else 0)
+                sys.stdout.write(str1 + str2 + '\n')
+                sys.stdout.flush()
+                start_time = time.time()
+
+            # Save some intermediate results in case of interruption.
+            if idx % save_iter == 0:
+                df.to_csv(
+                    os.path.join('.','submission/class_{}.csv'.format(class_type)),
+                    index=False)
+
+    sys.stdout.write('\n')
+    print df.head()
+
+    df.to_csv(
+        os.path.join('.', 'submission/class_{}.csv'.format(class_type)),
+        index=False)
---
+++ utils/align_images.py
@@ -0,0 +1,146 @@
+'''
+This file creates 
+'''
+
+import numpy as np
+import cv2
+from data_utils import resize
+import data_utils
+import os
+
+
+
+def convert_image_to_display(img):
+    '''
+    
+    :param img: 
+    :return: 
+    '''
+
+    return 255 * normalize_image(img)
+
+
+
+def calculate_warp_matrix(img1, img2, img_id):
+
+    img_size = img1.shape
+    img_size1 = img2.shape
+
+    nx = img_size[0]
+    ny = img_size[1]
+
+    nx_1 = img_size1[0]
+    ny_1 = img_size1[1]
+
+    if [nx, ny] != [nx_1, ny_1]:
+        img2 = resize(img2, [nx, ny])
+
+    # Crop the center area to avoid the boundary effect.
+    p1 = img1[int(nx * 0.2):int(nx * 0.8), int(ny * 0.2):int(ny * 0.8), :].astype(np.float32)
+    p2 = img2[int(nx * 0.2):int(nx * 0.8), int(ny * 0.2):int(ny * 0.8), :].astype(np.float32)
+
+    p1 = normalize_image(p1)
+    p2 = normalize_image(p2)
+
+    p1 = np.sum(p1, axis=2)
+    p2 = np.sum(p2, axis=2)
+
+    warp_mode = cv2.MOTION_EUCLIDEAN
+    warp_mat = np.eye(2, 3, dtype=np.float32)
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 1000, 1e-7)
+
+    try:
+        (cc, warp_mat) = cv2.findTransformECC(p1, p2, warp_mat, warp_mode, criteria)
+    except:
+        warp_mat = np.eye(2, 3, dtype=np.float32)
+        cc = 0.
+
+    print("Align two rasters for {}: cc:{}".format(img_id, cc))
+    print("warp matrix: ")
+    print warp_mat
+
+    return warp_mat
+
+
+
+def normalize_image(img):
+    '''
+    Normalize the image (10% to 90%) to [0,1]
+    :param img: 
+    :return: 
+    '''
+    img_shape = img.shape
+
+    if len(img_shape) == 3:
+        for channel in range(img_shape[2]):
+            im_min = np.percentile(img[:,:, channel], 10)
+            im_max = np.percentile(img[:,:, channel], 90)
+            img[:,:, channel] = (img[:,:, channel].astype(np.float32) - im_min) / (im_max - im_min)
+    elif len(img_shape) == 2:
+        im_min = np.percentile(img, 10)
+        im_max = np.percentile(img, 90)
+        img = (img.astype(np.float32) - im_min) / (im_max - im_min)
+
+    return img
+
+
+
+if __name__ == '__main__':
+
+    for phase in ['train', 'test']:
+
+        len_names = {'train': len(data_utils.all_train_names), 'test': len(data_utils.all_test_names)}[phase]
+
+        for id in range(len_names):
+
+            img = data_utils.ImageData(id, phase=phase)
+            img_id = img.image_id
+            images = img.read_image()
+
+            img_3 = images['3']
+            shape_out = img_3.shape[0:2]
+            img_m_orig = images['M']
+            img_a_orig = images['A']
+
+            warp_matrix_m = calculate_warp_matrix(img_3, img_m_orig, img_id + '_M')
+            warp_matrix_a = calculate_warp_matrix(img_3, img_a_orig, img_id + '_A')
+
+            np.save(open('image_alignment/{}_warp_matrix_m.npz'.format(img.image_id), 'w'), warp_matrix_m)
+            np.save(open('image_alignment/{}_warp_matrix_a.npz'.format(img.image_id), 'w'), warp_matrix_a)
+
+            # img.load_image()
+
+            img_m_register = data_utils.affine_transform(img_m_orig.astype(np.float32), warp_matrix_m, shape_out)
+            img_a_register = data_utils.affine_transform(img_a_orig.astype(np.float32), warp_matrix_a, shape_out)
+
+            img_3_rescale = convert_image_to_display(np.sum(img_3, 2))
+            img_m_orig_rescale = convert_image_to_display(np.sum(img_m_orig, axis=2))
+            img_a_orig_rescale = convert_image_to_display(np.sum(img_a_orig, axis=2))
+            img_m_register_rescale = convert_image_to_display(np.sum(img_m_register, axis=2))
+            img_a_register_rescale = convert_image_to_display(np.sum(img_a_register, axis=2))
+
+            empty_channel = np.zeros(shape_out).astype(np.uint8)
+
+            compare_m_orig = np.stack([img_m_orig_rescale, empty_channel, img_3_rescale], axis=-1).astype(np.uint8)
+            compare_m_register = np.stack([img_m_register_rescale, empty_channel, img_3_rescale], axis=-1).astype(np.uint8)
+
+            compare_a_orig = np.stack([img_a_orig_rescale, empty_channel, img_3_rescale], axis=-1).astype(np.uint8)
+            compare_a_register = np.stack([img_a_register_rescale, empty_channel, img_3_rescale], axis=-1).astype(np.uint8)
+
+            filenames = [
+                './image_alignment/comparison/{}/3_rescale.png'.format(img.image_id),
+                './image_alignment/comparison/{}/m_orig.png'.format(img.image_id),
+                './image_alignment/comparison/{}/m_register.png'.format(img.image_id),
+                './image_alignment/comparison/{}/a_orig.png'.format(img.image_id),
+                './image_alignment/comparison/{}/a_register.png'.format(img.image_id),
+            ]
+
+            for fn in filenames:
+                if not os.path.exists(os.path.dirname(fn)):
+                    os.mkdir(os.path.dirname(fn))
+
+            cv2.imwrite('./image_alignment/comparison/{}/3_rescale.png'.format(img.image_id),img_3_rescale[200:900,300:1200])
+            cv2.imwrite('./image_alignment/comparison/{}/m_orig.png'.format(img.image_id), compare_m_orig[200:900,300:1200,:])
+            cv2.imwrite('./image_alignment/comparison/{}/m_register.png'.format(img.image_id), compare_m_register[200:900,300:1200,:])
+            cv2.imwrite('./image_alignment/comparison/{}/a_orig.png'.format(img.image_id), compare_a_orig[200:900,300:1200,:])
+            cv2.imwrite('./image_alignment/comparison/{}/a_register.png'.format(img.image_id), compare_a_register[200:900,300:1200,:])
